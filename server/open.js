56f4dc3946fa98081842bf07dc03bfc1000a5c75
michaelkalai <michael.kalai124@gmail.com>
2024-03-23 18:13:46 -0400

    return response in function

--- server/open.js
+++ server/open.js
@@ -3,14 +3,16 @@ const { apikey } = require("./config.json")
 const openai = new OpenAI({ apiKey: apikey});
 
 async function main(question, date) {
-    prompt = "Given that the date is " + date + "you are identifying an events from announcements for a schedule, give me a description of the event that is less than 10 words and the date and time of the event. You should be describing one event. Here is the announcement: "
+    prompt = "Given that the date is " + date + "you are identifying an events from announcements for a schedule," + 
+    "give me a description of the event that is less than 10 words and the date and time of the event." + 
+    "You should be describing one event. Put your response in this format [event, date, starting time, location (put ~ if not given)]." + 
+    "If the announcement is not describing an upcoming event, respond with \"No Event\". Here is the announcement: "
     const completion = await openai.chat.completions.create({
       messages: [{ role: "user", content: prompt + question }],
       model: "gpt-3.5-turbo",
     });
   
-    console.log(completion.choices[0].message.content);
+    return completion.choices[0].message.content;
 }
 
-const question = "Good morning @everyone Casual Coding is today! At 5-7! In TURL011! Congrats to the OSC soccer team!! 2 games clean from mercy rule🤭 ⚽ Also as Rebecca mentioned in ⁠school, theres 8 spots left on the study abroad trip to Ghana for a Software Engineering and UX Design Internship (6 weeks, 6 credits)!!";
-main(question, "march 23rd ");
+module.exports = { main }
