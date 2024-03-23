337c91c98036279d06786e1ba0c88caf92554040
michaelkalai <michael.kalai124@gmail.com>
2024-03-23 15:07:12 -0400

    created discord bot

---
+++ server/bot.js
@@ -0,0 +1,25 @@
+
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+
+const bot = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+]
+});
+
+
+bot.once(Events.ClientReady, readyClient => {
+    console.log(`Logged in as ${bot.user.tag}`);
+});
+
+bot.on('messageCreate', message => {
+    if(message.channel.name == "announcements") {
+        console.log(message.content);
+    }
+});
+
+bot.login(token);
