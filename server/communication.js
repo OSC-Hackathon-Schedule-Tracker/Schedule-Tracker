4c0bf587714b1421fbf0ce3699761db4423dfeb2
michaelkalai <michael.kalai124@gmail.com>
2024-03-24 03:58:09 -0400

    modified file path

--- server/communication.js
+++ server/communication.js
@@ -8,7 +8,7 @@ app.get("/getschedule", (req, res) => {
 });
 
 
-const PORT = 3000;
+const PORT = 3002;
 app.listen(PORT, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3002");
 });
