7a99024e9fdbbeb3843121a5101ddbdb20a73c6d
Aaron Tran <dahpaladin123@gmail.com>
2024-03-24 02:02:16 -0400

    finished friont

--- schedule-website/src/App.js
+++ schedule-website/src/App.js
@@ -1,32 +1,36 @@
 import logo from './gator.png';
 import secondlogo from './image0.png'; 
-
-
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={secondlogo} className="App-secondlogo" alt="secondlogo" />
-        Welcome to the UF Club Info Hub!
-      </header>
-  
-      <div className="underlined-text">UPCOMING EVENTS AND ANNOUNCEMENTS</div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={secondlogo} className="App-secondlogo" alt="secondlogo" />
+          Welcome to the UF Club Info Hub! 
+        </header>
+    
+        <div className="underlined-text">UPCOMING EVENTS AND ANNOUNCEMENTS</div>
 
-      <div className="text-box-container">
-        <div className="text-box">Open Source Club: *event this, *event that</div>
-        <div className="text-box"></div>
-        <div className="text-box"></div>
-        <div className="text-box"></div>
-        <div className="text-box"></div>
-        <div className="text-box"></div>
+        <div className="text-box-container">
+          <div className="text-box">Open Source Club: *event this, *event that</div>
+          <div className="text-box">HAHAHAHAHAH</div>
+          <div className="text-box">JAJAJAJ</div>
+          <div className="text-box">KKAKAKAK</div>
+          <div className="text-box">AZASAD</div>
+          <div className="text-box">aSDSADASD</div>
+        </div>
       </div>
-    </div>
+
+      <footer></footer> {/* Move this line outside the .App div */}
+    </>
   );
 }
 
 export default App;
+
+
 
 
