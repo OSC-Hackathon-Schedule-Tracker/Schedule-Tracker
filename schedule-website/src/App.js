9ca8addc75c88bef7458a5c10721924ed6a5ca60
Aaron Tran <dahpaladin123@gmail.com>
2024-03-24 05:58:47 -0400

    finished product

--- schedule-website/src/App.js
+++ schedule-website/src/App.js
@@ -1,8 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import logo from './gator.png';
 import secondlogo from './image0.png'; 
 import './App.css';
 
 function App() {
+  const [schedule, setSchedule] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3002/getschedule')
+      .then(response => response.json())
+      .then(data => setSchedule(data));
+  }, []);
+
+  const formatEventData = (data) => {
+    return Object.entries(data).map(([clubName, events]) => (
+      <div key={clubName} className="text-box">
+        <strong>{clubName}:</strong>
+        <ul>
+          {events.map((event, index) => (
+            <li key={index}>
+              - {event[0]}, Date: {event[1]}, Time: {event[2]}, Location: {event[3] === '~' ? 'null' : event[3]}
+            </li>
+          ))}
+        </ul>
+      </div>
+    ));
+  };
+
   return (
     <>
       <div className="App">
@@ -11,25 +35,33 @@ function App() {
           <img src={secondlogo} className="App-secondlogo" alt="secondlogo" />
           Welcome to the UF Club Info Hub! 
         </header>
-    
+
         <div className="underlined-text">UPCOMING EVENTS AND ANNOUNCEMENTS</div>
 
         <div className="text-box-container">
-          <div className="text-box">Open Source Club: *event this, *event that</div>
-          <div className="text-box">HAHAHAHAHAH</div>
-          <div className="text-box">JAJAJAJ</div>
-          <div className="text-box">KKAKAKAK</div>
-          <div className="text-box">AZASAD</div>
-          <div className="text-box">aSDSADASD</div>
+          {Object.entries(schedule).length > 0 ? (
+            formatEventData(schedule)
+          ) : (
+            <>
+              <div className="text-box">Open Source Club: event this,event that</div>
+              <div className="text-box">HAHAHAHAHAH</div>
+              <div className="text-box">JAJAJAJ</div>
+              <div className="text-box">KKAKAKAK</div>
+              <div className="text-box">AZASAD</div>
+              <div className="text-box">aSDSADASD</div>
+            </>
+          )}
         </div>
       </div>
 
-      <footer></footer> {/* Move this line outside the .App div */}
+      <footer></footer>
     </>
   );
 }
 
 export default App;
+
+
 
 
 
--- server/communication.js
+++ server/communication.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const cors = require("cors"); // Import cors
 const app = express();
 const { getschedule } = require("./schedule");
 
-// needs frontend connection to finish testing
+// Enable CORS for all routes
+app.use(cors());
+
+// Route to get schedule
 app.get("/getschedule", (req, res) => {
+    console.log("getting schedule");
     res.json(getschedule());
 });
 
-
-const PORT = 3000;
+const PORT = 3002;
 app.listen(PORT, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port ${PORT}");
 });
